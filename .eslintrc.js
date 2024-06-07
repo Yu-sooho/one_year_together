0335d54d104792ac996e7f1eab556f46c327abf5
Yu-sooho <souho456@naver.com>
2024-06-07 12:39:37 +0900

    Feat: add eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,34 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-typescript',
+    'plugin:react/recommended',
+    'plugin:react-native/all',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['react', 'react-native', '@typescript-eslint'],
+  rules: {
+    // 프로젝트의 스타일 가이드에 맞게 규칙을 설정합니다.
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
 };
--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,35 @@
-import React, { useEffect } from 'react';
-import { StyleSheet, View, Image, Text } from 'react-native';
-import Animated, { useSharedValue, useAnimatedStyle, withTiming, Easing } from 'react-native-reanimated';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import fonts from './styles/fonts';
+import React from 'react'
+import {StyleSheet, View, Text} from 'react-native'
+import {NavigationContainer} from '@react-navigation/native'
+import {createNativeStackNavigator} from '@react-navigation/native-stack'
 
 function HomeScreen() {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Home Screen</Text>
     </View>
-  );
+  )
 }
 function HomeScreen2() {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Home Screen</Text>
     </View>
-  );
+  )
 }
 
-const Stack = createNativeStackNavigator();
-
+const Stack = createNativeStackNavigator()
 
 const App: React.FC = () => {
   return (
     <NavigationContainer>
-    <Stack.Navigator>
-      <Stack.Screen name="Home" component={HomeScreen} />
-      <Stack.Screen name="Hom2e" component={HomeScreen2} />
-    </Stack.Navigator>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Hom2e" component={HomeScreen2} />
+      </Stack.Navigator>
     </NavigationContainer>
-  );
-};
+  )
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -40,6 +37,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
-});
+})
 
-export default App;
+export default App
