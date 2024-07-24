82dc9c0cc2b815f60b2096ab27ec1dd0c87d3e88
Yu-sooho <souho456@naver.com>
2024-07-25 00:34:33 +0900

    Feat: package build

--- firebase/functions/.eslintrc.js
+++ firebase/functions/.eslintrc.js
@@ -13,19 +13,9 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: [
-    'airbnb-typescript',
-    'plugin:react/recommended',
-    'plugin:react-native/all',
-    'plugin:@typescript-eslint/recommended',
-    'prettier',
-    'prettier/@typescript-eslint',
-    'plugin:prettier/recommended',
-  ],
-  plugins: ['react', 'react-native', '@typescript-eslint'],
-  rules: {
-    // 프로젝트의 스타일 가이드에 맞게 규칙을 설정합니다.
-  },
+  extends: [],
+  plugins: [],
+  rules: {},
   settings: {
     react: {
       version: 'detect',
