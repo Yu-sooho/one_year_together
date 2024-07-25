046d9b7e09b3bd61c3e87a0ba68bebd250c215e7
Yu-sooho <souho456@naver.com>
2024-07-25 11:56:25 +0900

    Feat: changed style letter screen

--- src/screens/LetterScreen.tsx
+++ src/screens/LetterScreen.tsx
@@ -1,9 +1,20 @@
 import {RouteProp} from '@react-navigation/native'
 import {StackNavigationProp} from '@react-navigation/stack'
 import React from 'react'
-import {View, Text, Button, StyleSheet} from 'react-native'
+import {
+  View,
+  Text,
+  Button,
+  StyleSheet,
+  Dimensions,
+  ScrollView,
+} from 'react-native'
 import FastImage from 'react-native-fast-image'
-import {CustomBackgroundOpacity} from '../components'
+import {CustomBackgroundOpacity, CustomHeader} from '../components'
+import {SafeAreaView, useSafeAreaInsets} from 'react-native-safe-area-context'
+import colors from '../styles/colors'
+import {normalize} from '../utils'
+import fonts from '../styles/fonts'
 
 type LetterScreenNavigationProp = StackNavigationProp<
   LetterStackNavigatorParamList,
@@ -23,34 +34,56 @@ const LetterScreen: React.FC<Props> = ({navigation, route}) => {
   const {currentLetter} = route.params
   const {imageUrl, title, content} = currentLetter
 
+  const inset = useSafeAreaInsets()
+
+  const styles = StyleSheet.create({
+    container: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      width: Dimensions.get('window').width,
+      height: Dimensions.get('window').height + inset.top,
+    },
+    image: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      width: Dimensions.get('window').width,
+      height: Dimensions.get('window').height + inset.top,
+    },
+    contentContainer: {
+      width: Dimensions.get('window').width,
+      height: Dimensions.get('window').height + inset.top,
+      position: 'absolute',
+    },
+    contentText: {
+      ...fonts.bmjua16,
+      color: colors.cffffff,
+    },
+  })
   return (
     <View style={styles.container}>
-      <FastImage style={styles.image} source={{uri: imageUrl}}></FastImage>
-      <CustomBackgroundOpacity>
-        <Text>{title}</Text>
-        <Text>{content}</Text>
-        <Button
-          title="Go to Event List"
-          onPress={() => navigation.navigate('MainStackNavigator')}
+      <FastImage style={styles.image} source={{uri: imageUrl}} />
+      <CustomBackgroundOpacity />
+      <SafeAreaView style={styles.contentContainer}>
+        <CustomHeader
+          title={title}
+          containerStyle={{
+            backgroundColor: colors.transparent,
+          }}
+          iconColor={colors.cffffff}
+          titleStyle={{color: colors.cffffff}}
         />
-      </CustomBackgroundOpacity>
+        <ScrollView contentContainerStyle={{paddingTop: normalize(200)}}>
+          <View
+            style={{
+              alignItems: 'center',
+              paddingHorizontal: normalize(20),
+            }}>
+            <Text style={styles.contentText}>{content}</Text>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
     </View>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: '100%',
-    height: '100%',
-  },
-  image: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: '100%',
-    height: '100%',
-  },
-})
 
 export default LetterScreen
